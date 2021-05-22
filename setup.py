a515d457d55b0e8234798090500a569cd69df087
lobrien <lobrien@knowing.net>
2021-05-22 13:05:20 -1000

    Tweaks to strings

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description=''ike pono is a pipeline for identifying in-species individuals in photo catalogs. ',
-    author='Larry O'Brien',
+    description='\'ike pono is a pipeline for identifying in-species individuals in photo catalogs. ',
+    author='Larry O\'Brien',
     license='MIT',
 )
