952ec66eec2c5d1c6c5457136bcacb215fc7db78
Larry O'Brien <lobrien@knowing.net>
2024-08-09 15:46:02 -1000

    Added custom collate function for use with DataLoader

--- src/ikepono/labeledimageembedding.py
+++ src/ikepono/labeledimageembedding.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 from dataclasses import dataclass
 from pathlib import Path
@@ -9,9 +11,23 @@ class LabeledImageEmbedding:
     label: str
     source: Path
 
+
+
 # Note that the device on which the Tensor lives may be different from the device on which the model is running.
 @dataclass
 class LabeledImageTensor:
     image: torch.Tensor
     label: str
     source: Path
+
+    @staticmethod
+    def collate(batch):
+        images = torch.stack([item['image'] for item in batch])
+        labels = [item['label'] for item in batch]
+        sources = [item['source'] for item in batch]
+
+        return {
+            'images': images,
+            'labels': labels,
+            'sources': sources
+        }
